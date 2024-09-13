8efdaa576de3bd340c9b473c0bc83b4876bdb0bf
teerox44 <anthony@publiclyme.com>
2024-09-12 22:28:02 -0300

    setup configuration for application properties

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.example.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,4 +15,20 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	/*
+	This method is used to configure the CORS (Cross-Origin Resource Sharing) policy for the application.
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // Allow all paths
+						.allowedMethods("*") // Allow all HTTP methods
+						.allowedHeaders("*") // Allow all headers
+						.allowedOrigins("*"); // Allow all origins
+			}
+		};
+	}
 }
